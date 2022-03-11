c8fba0ee90dc072c71a1e09f8ff2833c756d684f
skent259 <skent259@gmail.com>
2022-03-10 20:39:26 -0600

    add sim files for 1.0, 2.0, 4.0

---
+++ sim/simulation-1.0.0_step-1.R
@@ -0,0 +1,183 @@
+##-----------------------------------------------------------------------------#
+#' Simulation 1.0.0
+#'   Testing MVT vs MVN (tail differences)
+#'   Step 1 - Grid-search cross-validation for optimal parameters
+#' 
+#' See simulation-spreadsheet.xlsx for details 
+##-----------------------------------------------------------------------------#
+
+library(tidyverse)
+library(here)
+library(glue)
+library(microbenchmark)
+library(moments)
+# devtools::install_github("skent259/mildsvm", ref = "dev-version")
+library(mildsvm)
+source(here("sim/utils.R"))
+
+## Command line arguments -----------------------------------------------------#
+#' @argument `i` the process number when using distributed computing
+#' @argument `batch_size` the number of models to run in this iteration
+args = commandArgs(trailingOnly = TRUE)
+print(args)
+
+i <- as.integer(args[1]) + 1
+batch_size <- as.integer(args[2])
+output_dir <- args[3]
+
+#' Set defaults for interactive session 
+set_default <- function(.x, val) { 
+  if(is.na(.x)) val else .x 
+}
+i <- set_default(i, 1)
+batch_size <- set_default(batch_size, 50)
+output_dir <- set_default(output_dir, "output/1.0")
+# 3780 runs at `batch_size` = 50, for 189,000 total
+
+## Output file ----------------------------------------------------------------#
+sim <- "1.0.0"
+step <- "1"
+output_fname <- glue("sim-{sim}-{step}-results_i={str_pad(i, 4, pad = 0)}.rds")
+output_fname <- here(output_dir, output_fname)
+gs_fname <- here(output_dir, glue("gridsearch_spec_{sim}.rds"))
+
+## Data set parameters --------------------------------------------------------#
+data_param <- expand_grid(
+  nbag = c(250, 100, 50),
+  ninst = c(3, 6),
+  nsample = c(20, 50),
+  ncov = 10, 
+  nimp_pos = list(1:5),
+  nimp_neg = list(1:5),
+  positive_prob = 0.15,
+  dist = list(c("mvt", 
+                "mvnormal", 
+                "mvnormal")),
+  degree = list(c(3, NA, NA)), 
+  mean = list(list(rep(0, 5), rep(0, 5), 0)),
+  sd_of_mean = list(rep(0.5, 3)),
+  cov = list(list(diag(1, nrow = 5), diag(1, nrow = 5), 1)),
+  sample_cov = FALSE,
+  replicate = 1:50
+)
+
+y <- "bag_label"
+bags <- "bag_name"
+
+nbag_test <- 500
+
+## Model parameters -----------------------------------------------------------#
+n_cols <- unique(data_param$ncov)
+.cost <- c(1, 10, 100)
+.sigma <- (1/n_cols) * 2 ^ c(-1, 0, 1)
+
+default_fns <- list(
+  mean = mean,
+  sd = sd
+)
+univariate_fns <- list(
+  skew = skewness,
+  kurt = kurtosis,
+  qtl25 = ~quantile(.x, 0.25),
+  qtl75 = ~quantile(.x, 0.75)
+)
+.fns <- list(
+  "baseline" = default_fns,
+  "univariate" = c(default_fns, univariate_fns)
+)
+
+model_param <-
+  bind_rows(
+    # mildsvm::mildsvm() 
+    expand_grid(fun = "mildsvm", 
+                cost = .cost,
+                method = c("qp-heuristic", "mip"),
+                control = transpose(expand_grid(
+                  kernel = "radial", 
+                  sigma = .sigma, 
+                  nystrom_args = transpose(nesting(m = 240, r = 240)),
+                  time_limit = 200,
+                  start = TRUE,
+                  verbose = FALSE
+                ))), 
+    # mildsvm::smm()
+    expand_grid(fun = "smm",
+                cost = .cost,
+                control = transpose(expand_grid(
+                  kernel = "radial", sigma = .sigma
+                ))),
+    # mildsvm::misvm()
+    expand_grid(fun = "misvm",
+                cost = .cost,
+                method = "heuristic",
+                .fns = .fns,
+                cor = c(TRUE, FALSE), 
+                control = transpose(expand_grid(
+                  kernel = "radial",
+                  sigma = .sigma
+                )))
+  )
+
+## CV parameters --------------------------------------------------------------#
+cv_param <- list(
+  nrep = 1,
+  nfolds = 1,
+  nfolds_gs = 5,
+  train = 1:nrow(data_param),
+  test = 1:nrow(data_param)
+)
+
+## Set up grid-search specification -------------------------------------------#
+set.seed(8)
+
+if (file.exists(gs_fname)) {
+  gridsearch_spec <- readRDS(gs_fname)
+} else {
+  gridsearch_spec <- define_gridsearch_specs(
+    y, bags,
+    cv_param, 
+    method = "train-test",
+    data_fun = mildsvm::generate_mild_df,
+    data_param = data_param
+  )
+  saveRDS(gridsearch_spec, gs_fname)
+}
+
+gs_spec_this_batch <- 
+  gridsearch_spec %>% 
+  select(train_name, test_name, gs_fold) %>% 
+  expand_grid(model_param) %>% 
+  slice(batch_index(i, batch_size)) %>%
+  left_join(gridsearch_spec, by = c("train_name", "test_name", "gs_fold"))
+# gs_spec_this_batch <- gs_spec_this_batch %>% group_by(fun, method, .fns, cor) %>% slice_head(n = 1)
+
+
+## Evaluate models in current batch -------------------------------------------#
+set.seed(8)
+out <- gs_spec_this_batch %>% 
+  transpose() %>% 
+  map_dfr(~evaluate_model(.x,
+                          train_name = .x$train_name,
+                          test_name = NULL,
+                          train = .x$train,
+                          test = .x$val,
+                          data_fun = mildsvm::generate_mild_df,
+                          data_param = data_param,
+                          nbag_test = nbag_test)) %>%
+  bind_cols(gs_spec_this_batch)
+
+## Save output ----------------------------------------------------------------#
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+print(out)
+out <- out %>% select(-train, -val)
+saveRDS(out, output_fname)
+
+
+
+
+
+
+
+
---
+++ sim/simulation-3.0.0_step-2.R
@@ -0,0 +1,141 @@
+##-----------------------------------------------------------------------------#
+#' Simulation 3.0.0
+#'   Testing Mean differences 
+#'   Step 2 - Test set evaluation on optimal parameters
+#' 
+#' See simulation-spreadsheet.xlsx for details 
+##-----------------------------------------------------------------------------#
+
+library(tidyverse)
+library(here)
+library(glue)
+library(microbenchmark)
+library(moments)
+# devtools::install_github("skent259/mildsvm", ref = "dev-version")
+library(mildsvm)
+source(here("sim/utils.R"))
+
+## Command line arguments -----------------------------------------------------#
+#' @argument `i` the process number when using distributed computing
+#' @argument `batch_size` the number of models to run in this iteration
+args = commandArgs(trailingOnly = TRUE)
+print(args)
+
+i <- as.integer(args[1]) + 1
+batch_size <- as.integer(args[2])
+output_dir <- args[3]
+
+#' Set defaults for interactive session 
+set_default <- function(.x, val) { 
+  if(is.na(.x)) val else .x 
+}
+i <- set_default(i, 1)
+batch_size <- set_default(batch_size, 100)
+output_dir <- set_default(output_dir, "output/3.0")
+# 1890 runs at `batch_size` = 100, for 189,000 total
+
+## Output file ----------------------------------------------------------------#
+sim <- "3.0.0"
+step <- "2"
+output_fname <- glue("sim-{sim}-{step}-results_i={str_pad(i, 4, pad = 0)}.rds")
+output_fname <- here(output_dir, output_fname)
+step1_fname <- glue("mildsvm-sims-results-{sim}-{step}.rds")
+step1_fname <- here(output_dir, step1_fname)
+
+
+## Data set parameters --------------------------------------------------------#
+data_param <- expand_grid(
+  nbag = c(50, 100, 250),
+  ninst = c(3, 6),
+  nsample = c(20, 50),
+  ncov = 10, 
+  nimp_pos = list(1:5),
+  nimp_neg = list(1:5),
+  positive_prob = 0.15,
+  dist = list(rep("mvnormal", 3)),
+  mean = list(list(
+    rep(0.3, 5),
+    rep(0, 5),
+    0
+  )),
+  sd_of_mean = list(rep(0.5, 3)),
+  cov = list(list(diag(1, nrow = 5), diag(1, nrow = 5), 1)),
+  sample_cov = FALSE,
+  replicate = 1:50
+)
+
+y <- "bag_label"
+bags <- "bag_name"
+
+nbag_test <- 500
+
+
+## Find optimal parameters from step 1 ----------------------------------------#
+gridsearch_spec <- readRDS(step1_fname)
+model_vars <- c("train_name", "test_name", "method", "fun", ".fns", "cor")
+gs_vars <- c("cost", "sigma")
+metric_to_optimize <- "auc"
+
+gridsearch_spec <- 
+  gridsearch_spec %>% 
+  hoist(control, "sigma", .remove = FALSE) %>% 
+  group_by(across(all_of(c(model_vars, gs_vars)))) %>% 
+  mutate(across(all_of(metric_to_optimize)), ~mean(.x, na.rm = TRUE)) %>% 
+  ungroup()
+
+## Set up test-set evaluation specification -----------------------------------#
+eval_spec <- 
+  gridsearch_spec %>% 
+  group_by(across(all_of(model_vars))) %>% 
+  mutate(across(time, ~sum(.x, na.rm = TRUE))) %>% 
+  slice_max(order_by = across(starts_with("mean_")), n = 1, with_ties = FALSE) %>% 
+  ungroup()
+
+eval_spec <- eval_spec %>% select(-auc, -auc_inst, -f1, -time, -mipgap, -sigma)
+eval_spec_this_batch <- slice(eval_spec, batch_index(i, batch_size))
+
+print(eval_spec)
+print(eval_spec_this_batch)
+
+# eval_spec <- 
+#   gridsearch_spec %>% 
+#   hoist(control, "sigma", .remove = FALSE) %>% 
+#   group_by(across(all_of(c(model_vars, gs_vars)))) %>%
+#   mutate(mean_auc = mean(auc)) %>% 
+#   ungroup() %>% 
+#   group_by(across(all_of(model_vars))) %>%
+#   mutate(sum_time = sum(time)) %>% 
+#   slice_max(order_by = mean_auc, n = 1, with_ties = FALSE) %>% 
+#   ungroup() %>% 
+#   select(-auc, -time, -mipgap, -sigma)
+
+## Evaluate models in current batch -------------------------------------------#
+set.seed(8)
+out <- eval_spec_this_batch %>% 
+  transpose() %>% 
+  map_dfr(~evaluate_model(.x,
+                          train_name = .x$train_name,
+                          test_name = .x$test_name,
+                          train = TRUE,
+                          test = TRUE,
+                          data_fun = mildsvm::generate_mild_df,
+                          data_param = data_param,
+                          nbag_test = nbag_test)) %>%
+  bind_cols(eval_spec_this_batch)
+
+
+## Save output ----------------------------------------------------------------#
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+print(out)
+out <- out %>% select(-train, -val)
+saveRDS(out, output_fname)
+
+
+
+
+
+
+
+
