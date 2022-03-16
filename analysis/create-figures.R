e055f514ad4aeae6a7d540c7daab2099d346f2bb
skent259 <skent259@gmail.com>
2022-03-16 16:30:27 -0500

    update create-figures.R
    - Include simulations from X.0.1
    - Add critical difference plot
    - Update analysis/utils.R accordingly

--- analysis/create-figures.R
+++ analysis/create-figures.R
@@ -6,13 +6,13 @@
 
 library(tidyverse)
 library(here)
+library(glue)
 library(mildsvm)
 source(here("analysis/utils.R"))
 
 res_dir <- "output"
 fig_dir <- "fig"
-# sims <- c("1.0.0", "2.0.0", "3.0.0", "4.0.0")
-sims <- c("3.0.0")
+sims <- c("1.0.0", "2.0.0", "3.0.0", "4.0.0", "1.0.1", "2.0.1", "3.0.1", "4.0.1")
 
 ## Pull in results ------------------------------------------------------------#
 
@@ -31,22 +31,30 @@ results_mod <- results %>%
                             "{ifelse(is.na(method), '', glue('{method}'))}",
                             "{ifelse(fun == 'misvm', glue(' {kernel} '), '')}", 
                             "{ifelse(fun == 'misvm', names(.fns), '')}",
-                            "{ifelse(fun == 'misvm', ifelse(cor, ' cor', ''), '')})")) %>% 
-  select(method_name, everything())
+                            "{ifelse(fun == 'misvm', ifelse(cor, ' cor', ''), '')})"),
+         sim = str_sub(sim, 1, 3)) %>% 
+  select(method_name, sim, everything())
 
 methods_to_show <- tribble(
   ~method, ~short_name, ~color, 
-  # "MISVM (qp-heuristic radial univariate cor)", "MI-SVM (univ1, univ2, cor)", "#13317DFF",
-  # "MISVM (qp-heuristic radial baseline cor)", "MI-SVM (univ1, cor)", "#434C99FF", 
-  # "MISVM (qp-heuristic radial univariate)", "MI-SVM (univ1, univ2)", "#5F66AFFF", 
-  # "MISVM (qp-heuristic radial baseline)", "MI-SVM (univ1)", "#7180C1FF", 
-  "MISVM (heuristic radial univariate cor)", "MI-SVM (univ1, univ2, cor)", "#13317DFF",
-  "MISVM (heuristic radial baseline cor)", "MI-SVM (univ1, cor)", "#434C99FF", 
-  "MISVM (heuristic radial univariate)", "MI-SVM (univ1, univ2)", "#5F66AFFF", 
-  "MISVM (heuristic radial baseline)", "MI-SVM (univ1)", "#7180C1FF", 
+  "MISVM (qp-heuristic radial univariate cor)", "MI-SVM (univ1, univ2, cor)", "#13317DFF",
+  "MISVM (qp-heuristic radial baseline cor)", "MI-SVM (univ1, cor)", "#434C99FF",
+  "MISVM (qp-heuristic radial univariate)", "MI-SVM (univ1, univ2)", "#5F66AFFF",
+  "MISVM (qp-heuristic radial baseline)", "MI-SVM (univ1)", "#7180C1FF",
+  # "MISVM (heuristic radial univariate cor)", "MI-SVM (univ1, univ2, cor)", "#13317DFF",
+  # "MISVM (heuristic radial baseline cor)", "MI-SVM (univ1, cor)", "#434C99FF",
+  # "MISVM (heuristic radial univariate)", "MI-SVM (univ1, univ2)", "#5F66AFFF",
+  # "MISVM (heuristic radial baseline)", "MI-SVM (univ1)", "#7180C1FF",
   "SMM ()", "SI-SMM", "#79D1D1FF",
   "MILDSVM (qp-heuristic)", "MI-SMM (heuristic)", "#EB7157FF",
   "MILDSVM (mip)", "MI-SMM (MIQP)", "#F5542CFF",
+)
+
+sim_labels <- c(
+  "1.0" = "1. Multivariate t vs multivariate normal",
+  "2.0" = "2. Covariance differences",
+  "3.0" = "3. Mean differences",
+  "4.0" = "4. Large covariance differences"
 )
 
 ## Results figure for each sim ------------------------------------------------#
@@ -56,7 +64,10 @@ for (s in str_sub(sims, 1, 3)) {
   p <- results_mod %>% 
     filter(str_sub(sim, 1, 3) == s) %>% 
     create_results_plot(methods_to_show) +
-    ggtitle(glue("Results of CV procedure across methods, simulation {sim}")) +
+    labs(
+      title = glue("Results of CV procedure across methods"),
+      subtitle = glue("{sim_labels[[s]]}")
+    ) +
     theme(plot.title.position = "plot")
   
   print(p)
@@ -66,7 +77,53 @@ for (s in str_sub(sims, 1, 3)) {
 
 
 
+## Critical difference plot ---------------------------------------------------#
+
+# Stratified ranks by the data set 
+rank_df <- 
+  results_mod %>%
+  filter(method_name %in% methods_to_show$method) %>% 
+  mutate(dataset = glue("sim-{sim}_{nsample}_{ninst}_{nbag}_id-{train_name}")) %>% 
+  group_by(dataset) %>% 
+  mutate(auc_rank = rank(-auc)) %>% 
+  group_by(method_name) %>% 
+  summarize(mean.rank = mean(auc_rank))
+
+cd_plot <- my_cd_plot(rank_df, methods_to_show)
+
+print(cd_plot)
+fname <- "toy-sim_average-rank-plot.pdf"
+ggsave(here(fig_dir, fname), cd_plot, width = 6, height = 3)
 
 
+# Faceted rank plot by simulation 
+rank_df <- 
+  results_mod %>%
+  filter(method_name %in% methods_to_show$method) %>% 
+  mutate(dataset = glue("sim-{sim}_{nsample}_{ninst}_{nbag}_id-{train_name}")) %>% 
+  group_by(dataset, sim) %>% 
+  mutate(auc_rank = rank(-auc)) %>% 
+  group_by(sim, method_name) %>% 
+  summarize(mean.rank = mean(auc_rank))
+
+cd_plot_facets <-
+  my_cd_plot(rank_df, methods_to_show) + 
+  facet_wrap(~sim, scales = "free", labeller = labeller(sim = sim_labels)) +
+  theme(strip.text = element_text(size = 14))
+
+print(cd_plot_facets)
+fname <- "toy-sim_average-rank-plot_faceted.pdf"
+ggsave(here(fig_dir, fname), cd_plot_facets, width = 12, height = 6)
 
 
+## Performance comparison for 1 data set size ---------------------------------#
+
+p_4scen <- results_mod %>% 
+  filter(nbag == 100, ninst == 3, nsample == 50) %>% 
+  create_results_plot(methods_to_show) + 
+  facet_wrap(~sim, labeller = labeller(sim = sim_labels), ncol = 1) +
+  labs(title = NULL)
+
+print(p_4scen)
+fname <- "toy-sim_comparison-of-4-scenarios.pdf"
+ggsave(here(fig_dir, fname), p_4scen, width = 5, height = 7)
--- analysis/utils.R
+++ analysis/utils.R
@@ -32,9 +32,15 @@ read_data_param <- function(sim,  results_dir = "output") {
 create_results_plot <- function(data, methods, facets = ninst + nsample ~ nbag) {
   data %>% 
     ggplot(aes(x = auc, y = method_name, color = method_name)) +
-    geom_quasirandom(aes(color = method_name, group = method_name), groupOnX = FALSE, alpha = 0.5) +
+    ggbeeswarm::geom_quasirandom(
+      aes(color = method_name, group = method_name),
+      groupOnX = FALSE, 
+      alpha = 0.5
+    ) +
     geom_vline(xintercept = 0.5, color = "grey40") +
-    geom_errorbarh(stat = "summary",fun.min = mean, fun.max = mean, color = "grey10") + 
+    geom_errorbarh(stat = "summary",
+                   fun.min = mean, fun.max = mean, 
+                   color = "grey10") + 
     facet_grid(facets, 
                labeller = label_both) + 
     scale_y_discrete(limits = methods$method,
@@ -47,4 +53,125 @@ create_results_plot <- function(data, methods, facets = ninst + nsample ~ nbag) 
     labs(title = "Comparing replications of CV procedure across methods", 
          x = "AUROC",
          y = NULL)
+}
+
+#' Plot critical differences
+#' 
+#' @param rank_df A data.frame with two columns `method_name`, which gives the
+#'   method and `mean.rank` which is the average rank across a number of data
+#'   sets or runs.
+#' @param methods A data.frame with key columns `method`, `short_name`, and
+#'   `color`, where `method` matches `data$method_name`, `short_name` gives the
+#'   display names, and `color` indicates which color to use for the points.
+#'   
+#' @return A ggplot object
+my_cd_plot <- function(rank_df, methods) {
+  # Set up data.frame for plot
+  obj <- list(
+    data = rank_df %>% 
+      mutate(
+        rank = rank(mean.rank),
+        right = ifelse(rank < (n() / 2 + 1), 0, 1), 
+        xend = ifelse(rank < (n() / 2 + 1), 0, max(rank)+1),
+        yend = ifelse(rank < (n() / 2 + 1), rank - 0.5, max(rank) - rank + 0.5)
+      ) %>% 
+      left_join(methods, by = c("method_name" = "method")) %>% 
+      rename(learner.id = short_name)
+  )
+  
+  
+  p <- ggplot(obj$data) +
+    geom_segment(y = 0, x = 0, xend = max(obj$data$xend), yend = 0) +
+    # geom_point(aes_string("mean.rank", 0, colour = "learner.id"), 
+    #            size = 3) +
+    geom_segment(aes_string("mean.rank", 0, xend = "mean.rank", 
+                            yend = "yend", color = "learner.id"),
+                 arrow = arrow(ends = "first", length = unit(0.1, "inches")),
+                 lineend = "round", linejoin = "mitre", 
+                 size = 1) +
+    geom_segment(aes_string("mean.rank", "yend", xend = "xend", 
+                            yend = "yend", color = "learner.id"), size = 1) +
+    geom_text(aes_string("xend", "yend", label = "learner.id", 
+                         hjust = "right"), color = "black", vjust = -1) +
+    xlab("Average Rank") +
+    scale_y_continuous(expand = expansion(mult = 0, add = c(0, 0.4))) +
+    scale_x_continuous(breaks = c(0:max(obj$data$xend))) + 
+    scale_color_manual(limits = methods$short_name,
+                       labels = methods$short_name,
+                       values = methods$color,
+                       guide = FALSE) +
+    theme_minimal() + 
+    theme(axis.text.y = element_blank(), 
+          axis.ticks.y = element_blank(), 
+          axis.title.y = element_blank(), 
+          axis.ticks.x = element_line(), 
+          legend.position = "none", 
+          panel.background = element_blank(), 
+          panel.border = element_blank(), 
+          # axis.line = element_line(size = 1), 
+          axis.line.y = element_blank(), 
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(), 
+          plot.background = element_blank())
+  
+  if (!is.null(obj$cd.info)) {
+    cd.x = obj$cd.info$x
+    cd.y = obj$cd.info$y
+    cd = obj$cd.info$cd
+    if (obj$cd.info$test == "bd") {
+      if (!is.null(baseline)) {
+        assertChoice(baseline, as.character(obj$data$learner.id))
+        cd.x = obj$data$mean.rank[obj$data$learner.id == 
+                                    baseline]
+      }
+      p = p + annotate("segment", x = cd.x + cd, xend = cd.x - 
+                         cd, y = cd.y, yend = cd.y, alpha = 0.5, color = "darkgrey", 
+                       size = 2)
+      p = p + annotate("segment", x = cd.x + cd, xend = cd.x + 
+                         cd, y = cd.y - 0.05, yend = cd.y + 0.05, color = "darkgrey", 
+                       size = 1)
+      p = p + annotate("segment", x = cd.x - cd, xend = cd.x - 
+                         cd, y = cd.y - 0.05, yend = cd.y + 0.05, color = "darkgrey", 
+                       size = 1)
+      p = p + annotate("point", x = cd.x, y = cd.y, alpha = 0.5)
+      p = p + annotate("text", label = stri_paste("Critical Difference =", 
+                                                  round(cd, 2), sep = " "), x = cd.x, y = cd.y + 0.05)
+    }
+    else {
+      nemenyi.data = obj$cd.info$nemenyi.data
+      if (!(nrow(nemenyi.data) == 0L)) {
+        p <- p + 
+          geom_segment(aes_string("xstart", "y", xend = "xend", 
+                                  yend = "y"), data = nemenyi.data, size = 1.5, color = "dimgrey", 
+                       alpha = 0.9) +
+          annotate(
+            "text", 
+            label = paste0("Critical Difference = ", round(cd, 2)),
+            y = max(obj$data$yend) + 0.5 + 0.15, 
+            x = mean(obj$data$mean.rank)
+          ) +
+          # annotate(
+          #   "segment",
+          #   x = mean(obj$data$mean.rank) - 0.5 * cd,
+          #   xend = mean(obj$data$mean.rank) + 0.5 * cd,
+          #   y = max(obj$data$yend) + 0.5 + 0.40,
+          #   yend = max(obj$data$yend) + 0.5 + 0.40,
+          #   size = 1L
+          # ) +
+          annotate(
+            "errorbar", 
+            xmin = mean(obj$data$mean.rank) - 0.5 * cd,
+            xmax = mean(obj$data$mean.rank) + 0.5 * cd,
+            y = max(obj$data$yend) + 0.5 + 0.40,
+            # yend = max(obj$data$yend) + 0.5 + 0.40,
+            width = 0.1, 
+            size = 1L
+          )
+      }
+      else {
+        message("No connecting bars to plot!")
+      }
+    }
+  }
+  return(p)
 }
